ab619f10e70634d0f8f54ddea255b00fab59be36
Francisco José Aragonés de la Rosa <62141854+FeelNostalgic@users.noreply.github.com>
2021-06-09 22:15:43 +0200

    Delete src directory

--- src/main/java/proyctosii/uma/daos/ExpedienteDao.java
+++
@@ -1,7 +0,0 @@
-package proyctosii.uma.daos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import proyctosii.uma.models.Expediente;
-
-public interface ExpedienteDao extends JpaRepository<Expediente,Integer> {
-}
